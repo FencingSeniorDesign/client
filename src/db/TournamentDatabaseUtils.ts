f63c884c713a9dd7133095ed1738c652f10924de
Luka Specter <lukaspecter@gmail.com>
2025-02-24 19:40:37 -0500

    update database schema with latest changes

--- src/db/TournamentDatabaseUtils.ts
+++ src/db/TournamentDatabaseUtils.ts
@@ -16,10 +16,11 @@ export async function initDB(): Promise<void> {
   // Tournaments
   try {
     await db.execAsync(`
-    CREATE TABLE IF NOT EXISTS Tournaments (
-      name TEXT PRIMARY KEY,
-      iscomplete integer default 0
-    );
+      create table IF NOT EXISTS Tournaments
+      (
+        name PRIMARY KEY,
+        iscomplete integer default 0
+      );
   `)
     console.log("Tournaments table initialized");
   } catch (error) {
@@ -29,22 +30,26 @@ export async function initDB(): Promise<void> {
   // Fencers
   try {
     await db.execAsync(`
-    create table if not exists Fencers (
-      id       integer primary key,
-      fname    text,
-      lname    text,
-      nickname text default null,
-      gender   text, -- so we can filter for gendered events(?)
-
-      -- Need ratings for seeding, assume null is unrated
-      erating  text CHECK (erating IN ('U', 'E', 'D', 'C', 'B', 'A')) default 'U',
-      eyear    integer,
-      frating  text CHECK (frating IN ('U', 'E', 'D', 'C', 'B', 'A')) default 'U',
-      fyear    integer,
-      srating  text CHECK (srating IN ('U', 'E', 'D', 'C', 'B', 'A')) default 'U',
-      syear    integer,
-
-      UNIQUE (fname, lname)
+      create table if not exists Fencers
+      (
+          id       integer primary key,
+          fname    text,
+          lname    text,
+          nickname text,
+          gender   text, -- so we can filter for genderd events(?)
+          club text,
+      
+          -- Need ratings for seeding, assume null is unrated
+          erating  text CHECK (erating IN ('U', 'E', 'D', 'C', 'B', 'A')) default 'U',
+          eyear    integer                                                default 0,
+          frating  text CHECK (frating IN ('U', 'E', 'D', 'C', 'B', 'A')) default 'U',
+          fyear    integer                                                default 0,
+          srating  text CHECK (srating IN ('U', 'E', 'D', 'C', 'B', 'A')) default 'U',
+          syear    integer                                                default 0,
+      
+      --     eradicate integer, (cate)
+      
+          UNIQUE (fname, lname)
       );
   `);
     console.log("Fencer table initialized");
@@ -55,12 +60,13 @@ export async function initDB(): Promise<void> {
   // Referees
   try {
     await db.execAsync(`
-    create table if not exists Referees (
-      id    integer PRIMARY KEY,
-      fname text,
-      lname text,
-      nickname text default null -- optional
-    );
+      create table if not exists Referees
+      (
+          id       integer PRIMARY KEY,
+          fname    text,
+          lname    text,
+          nickname text default null
+      );
   `);
     console.log("Referee table initialized");
   } catch (error) {
@@ -70,10 +76,11 @@ export async function initDB(): Promise<void> {
   // RoundTypes
   try {
     await db.execAsync(`
-    create table IF NOT EXISTS RoundTypes (
-      id         integer PRIMARY KEY,
-      name       text
-    );
+      create table IF NOT EXISTS RoundTypes
+      (
+          id   integer PRIMARY KEY,
+          name text check (name in ('pool', 'de'))
+      );
   `);
   } catch (error) {
     console.log(error);
@@ -82,15 +89,19 @@ export async function initDB(): Promise<void> {
   // Events
   try {
     await db.execAsync(`
-    CREATE TABLE IF NOT EXISTS Events (
-      id INTEGER PRIMARY KEY NOT NULL,
-      tname TEXT,
-      weapon TEXT,
-      gender TEXT,
-      age TEXT,
-      class TEXT,
-      seeding TEXT
-    );
+      create table if not exists Events
+      (
+          id      integer PRIMARY KEY,
+          tname   text NOT NULL,
+          weapon  text NOT NULL, -- epee, foil, saber
+          gender  text NOT NULL, -- mixed, male, female
+      
+          -- these should prob be turned into ints forign keyed to seperat etables
+          age     text NOT NULL, -- massive list I aint putting here rn
+          class   text NOT NULL, -- massive list I aint putting here rn
+          seeding text,          -- medium list I aint putting here rn
+          FOREIGN KEY (tname) references Tournaments (name)
+      );
   `);
     console.log("Events table initialized");
 
@@ -98,19 +109,68 @@ export async function initDB(): Promise<void> {
     console.log(error);
   }
 
+  // FencerEvents
+  try {
+    await db.execAsync(`
+      create table if not exists FencerEvents
+      (
+        fencerid integer,
+        eventid  integer,
+
+        FOREIGN KEY (fencerid) REFERENCES Fencers (id),
+        FOREIGN KEY (eventid) REFERENCES Events (id)
+        );
+    `);
+  } catch (error) {
+    console.log(error);
+  }
+
   // Rounds
   try {
     await db.execAsync(`
-    create table IF NOT EXISTS Rounds (
-      id      integer PRIMARY KEY,
-      eventid integer,
-      type    integer,
-      rorder  integer, -- round 1 is fenced first, then round 2, etc. Might be able to just use the id. Depends how annoying the update query is
-
-      FOREIGN KEY (eventid) references Events (id),
-      FOREIGN KEY (type) references RoundTypes (id)
+      create table IF NOT EXISTS Rounds
+      (
+          id          integer PRIMARY KEY,
+          eventid     integer,
+          type        integer,
+          rorder      integer, -- round 1 is fenced first, then round 2, etc
+      
+      
+          -- Pool Settings
+          promotionpercent integer default 100,
+          targetbracket integer,
+      
+          -- DE settings
+          deformat    TEXT,    -- only used for DE rounds (e.g. 'single', 'double', 'compass')
+          detablesize INTEGER, -- only used for DE rounds
+      
+          isstarted integer check (iscomplete in (0, 1)) default 0, -- prevent edits when the round starts
+          iscomplete  integer check (iscomplete in (0, 1)) default 0,
+      
+          FOREIGN KEY (eventid) references Events (id),
+          FOREIGN KEY (type) references RoundTypes (id)
       );
   `)
+  } catch (error) {
+    console.log(error);
+  }
+
+  // FencerPoolAssignment
+  try {
+    await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS FencerPoolAssignment
+      (
+        roundid INTEGER NOT NULL,
+        poolid   INTEGER NOT NULL,
+        fencerid INTEGER NOT NULL,
+        fenceridinpool integer, -- The number refs call
+
+        PRIMARY KEY (roundid, poolid, fencerid),
+        FOREIGN KEY (roundid) REFERENCES Rounds (id),
+        FOREIGN KEY (fencerid) REFERENCES Fencers (id)
+        --     FOREIGN KEY (poolid) REFERENCES Pools (id)
+        );
+    `)
   } catch (error) {
     console.log(error);
   }
@@ -118,38 +178,42 @@ export async function initDB(): Promise<void> {
   // Bouts
   try {
     await db.execAsync(`
-    create table if not exists Bouts (   -- Bouts are essentially just the connection of a bunch of existing data. Only new thing here should be the id
-        id           integer PRIMARY KEY,
-        lfencer      integer,
-        rfencer      integer,
-        referee      integer,
-        eventid      integer,
-        roundid      integer,
-    
-        FOREIGN KEY (lfencer) REFERENCES FencerPoolID (poolid),
-        FOREIGN KEY (rfencer) REFERENCES FencerPoolID (poolid),
-        FOREIGN KEY (referee) REFERENCES Referees (id),
-        FOREIGN KEY (eventid) REFERENCES Events (id),
-        FOREIGN KEY (roundid) REFERENCES Rounds (id)
-    );
+      create table if not exists Bouts
+      (
+          id        integer PRIMARY KEY,
+          lfencer   integer,
+          rfencer   integer,
+          victor    integer,
+          referee   integer,
+          eventid   integer,
+          roundid   integer,
+      
+          tableof integer check ( tableof in (2,4,8,16,32,64,128,256)), -- Only used for DEs. Specifies which table the fencer is in
+      
+          FOREIGN KEY (lfencer) REFERENCES Fencers (id),
+          FOREIGN KEY (rfencer) REFERENCES Fencers (id),
+          FOREIGN KEY (victor) REFERENCES Fencers (id),
+          FOREIGN KEY (referee) REFERENCES Referees (id),
+          FOREIGN KEY (eventid) REFERENCES Events (id),
+          FOREIGN KEY (roundid) REFERENCES Rounds (id)
+      );
   `)
   } catch (error) {
     console.log(error);
   }
 
-  // FencerPoolId
+  // DETable
   try {
     await db.execAsync(`
-    create table if not exists FencerPoolID (   -- We need this because many fencers can be in many pools while having different pool ids.
-        fencerid integer,
+      create table if not exists DETable
+      (
+        id integer PRIMARY KEY,
         roundid integer,
-        poolid integer, -- Is the number that refs call when saying who is fencing / up next. order doesn't matter
-    
-        PRIMARY KEY (fencerid, roundid),
-        FOREIGN KEY (fencerid) REFERENCES Fencers (id),
-        FOREIGN KEY (roundid) REFERENCES Rounds (id)
-    );
-  `)
+        tableof integer check ( tableof in (2,4,8,16,32,64,128,256)),
+
+        FOREIGN KEY (roundid) REFERENCES Rounds(id)
+        );
+    `)
   } catch (error) {
     console.log(error);
   }
@@ -157,47 +221,57 @@ export async function initDB(): Promise<void> {
   // FencerBouts
   try {
     await db.execAsync(`
-    create table if not exists FencerBouts (
-      boutid   integer,
-      fencerid integer,
-      score    integer,
-  
-      PRIMARY KEY (boutid, fencerid),
-      FOREIGN KEY (boutid) REFERENCES Bouts (id),
-      FOREIGN KEY (fencerid) REFERENCES Fencers (id)
-    );
+      create table if not exists FencerBouts
+      (
+          boutid   integer,
+          fencerid integer,
+          score    integer,
+      
+          PRIMARY KEY (boutid, fencerid),
+          FOREIGN KEY (boutid) REFERENCES Bouts (id),
+          FOREIGN KEY (fencerid) REFERENCES Fencers (id)
+      );
   `)
   } catch (error) {
     console.log(error);
   }
-  // FencerEvents
+
+  // CreateFencerBoutsTrigger
   try {
     await db.execAsync(`
-    create table if not exists FencerEvents (
-      fencerid integer,
-      eventid  integer,
+      CREATE TRIGGER create_fencer_bouts_after_bout_insert
+          AFTER INSERT
+          ON Bouts
+      BEGIN
+          -- Insert a FencerBouts row for the left fencer.
+          INSERT INTO FencerBouts (boutid, fencerid)
+          VALUES (NEW.id, NEW.lfencer);
+      
+          -- Insert a FencerBouts row for the right fencer.
+          INSERT INTO FencerBouts (boutid, fencerid)
+          VALUES (NEW.id, NEW.rfencer);
+      END;
+  `)
 
-      PRIMARY KEY (fencerid, eventid),
-      FOREIGN KEY (fencerid) REFERENCES Fencers (id),
-      FOREIGN KEY (eventid) REFERENCES Events (id)
-      );
-  `);
   } catch (error) {
     console.log(error);
   }
 
-  // EventRounds
+  // SeedingFromRoundResults
   try {
     await db.execAsync(`
-    create table if not exists EventRounds (
-      eventid integer,
-      roundid integer,
-      iscomplete integer,
-  
-      PRIMARY KEY (eventid, roundid),
-      FOREIGN KEY (eventid) REFERENCES Events (id),
-      FOREIGN KEY (roundid) REFERENCES Rounds (id)
-    );
+      create table if not exists SeedingFromRoundResults
+      (
+          id       integer PRIMARY KEY,
+          fencerid integer,
+          eventid  integer,                                          --TODO do we need this if we use FencerEvents?
+          roundid  integer,
+          seed     integer,
+      
+          FOREIGN KEY (fencerid) REFERENCES FencerEvents (fencerid), -- Use FencerEvents so that seeding is limited to the round
+          FOREIGN KEY (roundid) REFERENCES Rounds (id),
+          FOREIGN KEY (eventid) REFERENCES Events (id)
+      );
   `)
   } catch (error) {
     console.log(error);
@@ -244,10 +318,10 @@ export async function dbInsertExampleData(): Promise<void> {
     syear: 2025,
   }
 
-  dbCreateFencerByName(Guy1)
-  dbCreateFencerByName(Guy2)
-  dbCreateFencerByName(Guy3)
-  dbCreateFencerByName(Guy4)
+  await dbCreateFencerByName(Guy1)
+  await dbCreateFencerByName(Guy2)
+  await dbCreateFencerByName(Guy3)
+  await dbCreateFencerByName(Guy4)
 }
 
 // TODO - Pass tournament object from Types instead
